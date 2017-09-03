53bb53efd19a4f753d3d0cccb5db2ab3e27a214d
Evan Turner <evturn@gmail.com>
2017-09-02 22:50:52 -0400

    Add reactotron

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
+import Reactotron, {
+  trackGlobalErrors,
+  openInEditor,
+  overlay,
+  asyncStorage,
+  networking
+} from 'reactotron-react-native'
 import React from 'react'
 import Accessor from './app/index.js'
+
+ Reactotron
+  .configure({
+    name: 'Accessor', 
+  })
+  .use(trackGlobalErrors())
+  .use(openInEditor())
+  .use(overlay())
+  .use(asyncStorage())
+  .use(networking())
+  .connect()
 
 class App extends React.Component {
   render() {
